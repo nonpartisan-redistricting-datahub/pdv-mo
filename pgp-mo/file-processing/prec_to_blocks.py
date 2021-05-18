41754990da1208ff6eef84f5c4d627bc9d1a8524
Sam Spinner <spinners@stanford.edu>
2021-05-18 12:27:38 -0700

    adding pgp for mo

---
+++ pgp-mo/file-processing/prec_to_blocks.py
@@ -0,0 +1,21 @@
+import areal_interpolation as ai
+import geopandas as gpd
+import maup
+import matplotlib
+
+small = gpd.read_file('./raw-from-source/tabblock2010_29_pophu/tabblock2010_29_pophu.shp')
+large = gpd.read_file('./raw-from-source/tl_2013_29_bg/tl_2013_29_bg.shp')
+# large = gpd.read_file('/Users/hopecj/projects/gerryspam/MO/dat/mo_prec_labeled/mo_prec_labeled_nopop.shp')
+
+small = small.to_crs(large.crs)
+
+# target_columns should be name of disaggregation column from `large`
+# start time: 12:31 pm
+# end time: 1:30 pm
+agg = ai.aggregate(small, large, target_columns=['GEOID'])[0] # this takes a while!
+
+agg = agg[['STATEFP10', 'COUNTYFP10', 'TRACTCE10', 'BLOCKCE',
+       'BLOCKID10', 'PARTFLG', 'HOUSING10',
+       'POP10', 'GEOID', 'geometry']]
+
+agg.to_file('./output/blocks_with_bg/blocks_with_bg.shp')
